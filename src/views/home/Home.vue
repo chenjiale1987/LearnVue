96f10afec21d055a7978e0b5fa6731ec1945f205
chenjiale <chenjiale1987@sina.com>
2020-09-12 12:48:46 +0800

    '添加本周流行和选项卡'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,20 +5,69 @@
     </nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"/>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
   import HomeSwiper from './childComps/HomeSwiper'
   import RecommendView from './childComps/RecommendView'
+  import FeatureView from './childComps/FeatureView'
+
+  import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from 'components/content/tabControl/TabControl'
+
   import {getHomeMultidata} from 'network/home'
   export default {
     name: "Home",
     components:{
-      NavBar,
       HomeSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView,
+      NavBar,
+      TabControl
     },
     data(){
       return {
@@ -37,8 +86,21 @@
 </script>
 
 <style scoped>
+  #home{
+    padding-top:44px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color:#fff;
+    position:fixed;
+    left:0;
+    right:0;
+    top:0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
   }
 </style>
