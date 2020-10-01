01a56f3159c7063cee1514980f7327a111f9305f
chenjiale <chenjiale1987@sina.com>
2020-10-01 22:49:33 +0800

    '详情页店铺信息'

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -3,6 +3,7 @@
       <detail-nav-bar/>
       <detail-swiper :top-images="topImages"/>
       <detail-base-info :goods="goods"/>
+      <detail-shop-info :shop="shop"/>
   </div>
 </template>
 
@@ -10,20 +11,23 @@
   import DetailNavBar from './childComps/DetailNavBar'
   import DetailSwiper from './childComps/DetailSwiper'
   import DetailBaseInfo from './childComps/DetailBaseInfo'
-  import {getDetail,Goods} from 'network/detail'
+  import DetailShopInfo from './childComps/DetailShopInfo'
+  import {getDetail,Goods,Shop} from 'network/detail'
   
   export default {
     name: "Detail",
     components:{
         DetailNavBar,
         DetailSwiper,
-        DetailBaseInfo
+        DetailBaseInfo,
+        DetailShopInfo
     },
     data(){
         return {
             id:null,
             topImages:[],
-            goods:{}
+            goods:{},
+            shop:{}
         }
     },
     created(){
@@ -32,6 +36,7 @@
             const data=res.result;
             this.topImages=data.itemInfo.topImages;
             this.goods=new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+            this.shop=new Shop(data.shopInfo)
         })
 
     }
