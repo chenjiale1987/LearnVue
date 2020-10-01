6727dc3c5ad75a4503e25f30f913296341823574
chenjiale <chenjiale1987@sina.com>
2020-10-01 15:19:38 +0800

    '添加详情跳转和导航'

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="detail">
+      <detail-nav-bar/>
+  </div>
+</template>
+
+<script>
+  import DetailNavBar from './childComps/DetailNavBar'
+  export default {
+    name: "Detail",
+    components:{
+        DetailNavBar
+    },
+    data(){
+        return {
+            id:null
+        }
+    },
+    created(){
+        this.id=this.$route.params.id
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
