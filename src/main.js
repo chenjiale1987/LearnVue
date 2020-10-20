3dd48f66365ab78445e32a04b3e33c4f11c86bd6
chenjiale <chenjiale1987@sina.com>
2020-10-20 23:32:23 +0800

    '添加图片懒加载'

--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import FastClick from 'fastclick'
 
 import toast from 'components/common/toast'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 
 Vue.use(toast)
+
+FastClick.attach(document.body)
+Vue.use(VueLazyLoad,{
+  preLoad: 1,
+  loading: require('assets/img/common/placeholder.png')
+}) 
 
 new Vue({
   render: h => h(App),
